bf6c61ac6b4d0f7f4a37ce96149e1a9724dda55c
StevenFMl <stevenflores2011@hotmail.com>
2024-08-30 00:31:40 -0500

    Confirmado

--- src/app/pages/secure/profile/edit/edit.page.ts
+++ src/app/pages/secure/profile/edit/edit.page.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
+import { AuthService } from 'src/app/services/auth/auth.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-import { ActionSheetController, NavController } from '@ionic/angular';
 import { ToastService } from 'src/app/services/toast/toast.service';
+import { ActionSheetController, NavController } from '@ionic/angular';
+import { Router } from '@angular/router';
+//import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+
 
 @Component({
   selector: 'app-edit',
@@ -14,55 +18,140 @@ export class EditPage implements OnInit {
   edit_profile_form: FormGroup;
   submit_attempt: boolean = false;
 
+  txt_nombre:string="";
+  txt_apellido:string="";
+  txt_cedula:string="";
+  persona: string="";
+  correo: string="";
+  nombre: string="";
+  apellido: string="";
+  cedula: string="";
+  imgUrl: any = null;
+  img: any = null;
+
+
   constructor(
+    private authService: AuthService,
     private formBuilder: FormBuilder,
     private toastService: ToastService,
     private navController: NavController,
-    private actionSheetController: ActionSheetController
-  ) { }
+    private actionSheetController: ActionSheetController,
+    private router: Router) { 
+        
+      this.authService.getSession('persona').then((res:any)=>{
+        this.persona=res;
+        
+        });
+      this.authService.getSession('correo').then((res:any)=>{
+          this.correo=res;
+          
+          });
+          this.authService.getSession('nombre').then((res:any)=>{
+            this.nombre=res;
+            
+            });
+            this.authService.getSession('apellido').then((res:any)=>{
+              this.apellido=res;
+              
+              });
+              this.authService.getSession('cedula').then((res:any)=>{
+                this.cedula=res;
+                
+                });
+                this.authService.getSession('imgUrl').then((res: any) => {
+                  this.imgUrl = res;
+                });
+        }
 
   ngOnInit() {
 
     // Setup form
     this.edit_profile_form = this.formBuilder.group({
-      name_first: ['', Validators.required],
-      name_last: ['', Validators.required]
+      nombre: ['', Validators.required],
+      apellido: ['', Validators.required]
+
     });
 
-    // DEBUG: Prefill inputs
-    this.edit_profile_form.get('name_first').setValue('John');
-    this.edit_profile_form.get('name_last').setValue('Doe');
+
   }
+
+  async actnombre() {
+  const nombreImg = 'avatar_user_' + this.cedula; // Nombre de la imagen
+  let datos = {
+    accion: "actnombre",
+    cedula: this.cedula,
+    nombre: this.txt_nombre ? this.txt_nombre : this.nombre,
+    apellido: this.txt_apellido ? this.txt_apellido : this.apellido,
+    imgUrl: 'https://dominant-crow-certainly.ngrok-free.app/WsMunicipioIonic/uploads/' + nombreImg + '.jpg',
+  };
+  
+  this.authService.postData(datos).subscribe((res: any) => {
+    if (res.estado === true) {
+      this.authService.showToast2(res.mensaje2);
+      // Navegar al home
+      this.navController.navigateRoot('/home');
+    } else {
+      this.authService.showToast(res.mensaje);
+    }
+  });
+
+  if (this.img != null) {
+    const formData = new FormData();
+    formData.append('image', this.img);
+    formData.append('userId', nombreImg);
+
+    this.authService.uploadIMG(formData).subscribe(
+      (res: any) => {
+        this.authService.showToast2(res.mensaje);
+      },
+      (error) => {
+        console.error(error);
+        this.authService.showToast('Error al subir la imagen');
+      }
+    );
+  }
+}
 
   // Update profile picture
   async updateProfilePicture() {
-
     const actionSheet = await this.actionSheetController.create({
-      header: 'Choose existing picture or take new',
+      header: 'Elija una foto existente ',
       cssClass: 'custom-action-sheet',
       buttons: [
         {
-          text: 'Choose from gallery',
+          text: 'Elegir de la galería ',
           icon: 'images',
           handler: () => {
-            // Put in logic ...
-          }
+            const inputElement = document.createElement('input');
+            inputElement.type = 'file';
+            inputElement.accept = 'image/*';
+            inputElement.click();
+  
+            inputElement.addEventListener('change', async (event: any) => {
+              const file = event.target.files[0];
+              this.img = file;
+              if (file) {
+                const reader = new FileReader();
+                reader.onload = async (e: any) => {
+                  this.imgUrl = e.target.result;
+                };
+                reader.readAsDataURL(file);
+              }
+            });
+          },
         },
         {
-          text: 'Take picture',
-          icon: 'camera',
-          handler: () => {
-            // Put in logic ...
-          }
-        }, {
-          text: 'Cancel',
+          text: 'Cancelar',
           icon: 'close',
-          role: 'cancel'
-        }]
+          role: 'cancel',
+        },
+      ],
     });
     await actionSheet.present();
   }
 
+
+  
   // Submit form
   submit() {
 
@@ -74,14 +163,13 @@ export class EditPage implements OnInit {
       // Save form ...
 
       // Display success message and go back
-      this.toastService.presentToast('Success', 'Profile saved', 'top', 'success', 2000);
+      this.toastService.presentToast('Guardado', 'Perfil guardado correctamente', 'top', 'success', 2000);
       this.navController.back();
 
     } else {
 
       // Display error message
-      this.toastService.presentToast('Error', 'Please fill in all required fields', 'top', 'danger', 2000);
+      this.toastService.presentToast('Error', 'Por favor, llene todos los campos', 'top', 'danger', 2000);
     }
   }
-
 }
